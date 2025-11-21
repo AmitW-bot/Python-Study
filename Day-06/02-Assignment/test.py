503d0e51921635c64d562d33702a2404a5225baf
Ayushman-1744 <ayushman1744@gmail.com>
2025-11-21 09:01:36 +0000

    new file

---
+++ Day-06/02-Assignment/test.py
@@ -0,0 +1,65 @@
+#a=3
+#b=4
+
+#sum=a+b
+#difference=a-b
+#product=a*b
+#quotient=a/b
+#modulus=a%b
+#exponentiation=a**b
+#floor_division=a//b
+
+#greater=a>b
+#less=a<b
+#greater_equal=a>=b
+#less_equal=a<=b
+#equal=a==b
+#not_equal=a!=b
+
+#x=True
+#y=False
+
+#and_1=x and y
+#or_1=x or y
+#not_1= not y
+#not_2= not x
+
+#total =10
+
+#total +=4
+#total -=7
+#total *=8
+#total /=2
+
+#print("Sum of function is=",sum)
+#print("difference of function is=",difference)
+#print("product of function is=",product)
+#print("quotient of function is=",quotient)
+#print("modulus of function is=",modulus)
+#print("exponentiation of function is=",exponentiation)
+#print("floor division of function is=",floor_division)
+
+#print("Greater than function is=",greater)
+#print("Less than function is=",less)
+#print("Greater than or equal to function is=",greater_equal)
+#print("Less than or equal to function is=",less_equal)
+#print("Equal to function is=",equal)
+#print("Not equal to function is=",not_equal)
+
+#print("Logical and is", and_1)
+#print("Logical or is", or_1)
+#print("Logical not is", not_1 )
+#print("Logical not is", not_2 )
+
+#print("final value of total is=",total)
+
+#my_list=["apple","grapes","rabbit",1,2,3,4]
+#u="apple"
+
+#b="grapes"
+#c="banana"
+
+#print(u is u)
+#print(u is not my_list)
+#print(b in my_list)
+#print(c not in my_list)

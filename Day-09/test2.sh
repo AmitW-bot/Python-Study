d85484fa73f26a9a379225e5619627afbec2dccc
Ayushman-1744 <ayushman1744@gmail.com>
2025-11-24 07:11:26 +0000

    new test case

---
+++ Day-09/test2.sh
@@ -0,0 +1,31 @@
+servers=("server1" "server2" "server3")
+for server in "${servers[@]}"; do
+    check_resource_utilization "$server"
+done
+
+while ! aws ec2 describe-instance-status --instance-ids i-1234567890abcdef0 | grep -q "running"; do
+    echo "Waiting for the EC2 instance to be running..."
+    sleep 10
+done
+
+while true; do
+    if tail -n 1 /var/log/app.log | grep -q "ERROR"; then
+        send_alert "Error detected in the log."
+    fi
+    sleep 5
+done
+
+while true; do
+    replication_lag=$(mysql -e "SHOW SLAVE STATUS\G" | grep "Seconds_Behind_Master" | awk '{print $2}')
+    if [ "$replication_lag" -gt 60 ]; then
+        trigger_data_sync
+    fi
+    sleep 60
+done
+
+while true; do
+    if ! check_service_health; then
+        restart_service
+    fi
+    sleep 30
+done

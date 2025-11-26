870cbd441140ef1f8972708ba43704f8dd2ed240
Pramod Chand <pramodchand@gmail.com>
2025-11-26 10:47:00 +0545

    Use re.findall to find all matches of a pattern
    Replaced re.search with re.findall to find all occurrences of the pattern in the text and added a count of the occurrences.

--- Day-02/examples/03-regex-findall.py
+++ Day-02/examples/03-regex-findall.py
@@ -3,8 +3,9 @@ import re
 text = "The quick brown fox"
 pattern = r"brown"
 
-search = re.search(pattern, text)
+search = re.findall(pattern, text)
 if search:
-    print("Pattern found:", search.group())
+    print("Pattern found:", search)
+    print("Pattern count:", len(search))
 else:
     print("Pattern not found")
